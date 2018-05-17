da4ef1de01ddc9e07c30aa21983316e1b4e61ebb
Gordon Pont <gordon@pont.live>
2018-05-17 07:39:28 -0600

    2018-05-17 - Updated Header Per Instructions

--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The main() class file for the cityOfAaron Project
+ * CIT-260
+ * Spring 2018
+ * Team Members: Gordon Pont, McKay Pingel, Mitchell Thomas
  */
 package cityofaaron;
 
